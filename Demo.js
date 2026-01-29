4e8ec1973aa2e93a71380c41e1f28dd657a23fc9
Mukul <mukulkashyap.business@gmail.com>
2026-01-29 15:58:25 +0530

    data types

---
+++ Demo.js
@@ -0,0 +1,14 @@
+let a =10; //number
+let str="Hello"; //string
+let isok = true; //boolean
+let arr = [1,2,3]; //object
+let obj = {name:"Dennis", age:20}; //object
+let empty = null; //object
+let b; //undefined
+console.log(typeof a);
+console.log(typeof str);
+console.log(typeof isok);
+console.log(typeof arr);
+console.log(typeof obj);
+console.log(typeof empty);
+console.log(typeof b);
