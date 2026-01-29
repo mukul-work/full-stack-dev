4f48f0bd3417f6fa965fbfd5d356e172d07c2b3b
Mukul <mukulkashyap.business@gmail.com>
2026-01-29 16:14:12 +0530

    practice: equality operator and loops

---
+++ loops.js
@@ -0,0 +1,24 @@
+for(let i = 0; i < 4; i++){
+    console.log("for loop");
+}
+
+let j = 0;
+while(j < 4){
+    console.log("while loop");
+    j++;
+}
+
+let k = 0;
+do{
+    console.log("do-while loop");
+    k++;
+}while(k<4);
+
+let arr = [1,2,3,4]
+for(let x in arr){
+    console.log(arr[x]);
+}
+
+arr.forEach(function(num){
+    console.log(num);
+})
