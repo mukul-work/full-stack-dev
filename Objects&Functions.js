45e003b23071a212517f8b1580f850367ddfedf1
Mukul <mukulkashyap.business@gmail.com>
2026-01-30 14:52:35 +0530

    add: object practice

--- Objects&Functions.js
+++ Objects&Functions.js
@@ -3,3 +3,13 @@ console.log(obj.name);
 
 obj.name = "ritchie";
 console.log(obj.name);
+
+const sales = "Toyota";
+function Types(name){
+    return (name === "Honda")? name : "Sorry";
+}
+
+const car = {myCar : "Saturn", getCar : Types("Maruti"), special : sales};
+console.log(car.myCar);
+console.log(car.getCar);
+console.log(car.special);
