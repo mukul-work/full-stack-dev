797fcf83698c6ff3a26a1ffc373b1ac8814bd43a
Mukul <mukulkashyap.business@gmail.com>
2026-02-08 22:27:50 +0530

    add: topics covered in class

---
+++ app/vit-app/react-notes/src/data/topics.js
@@ -0,0 +1,14 @@
+export const topics = [
+    {
+        title: "Creating Components",
+        description: "Breaking UI into reusable peices like Header, Nav, Footer."
+    },
+    {
+        title: "Using Props",
+        description: "Passing data from parent to child components."
+    },
+    {
+        title: "React Structure",
+        description: "Understanding App.jsx, main.jsx, and components hierarchy."
+    }
+];
