9e205d4269ac13e082f170b266cdcd6c46f2eb6e
Mukul <mukulkashyap.business@gmail.com>
2026-02-12 15:29:35 +0530

    Initial Commit

---
+++ app/vit-app/app12feb/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import EventToDo from './EventToDo'
+
+function App() {
+
+  return (
+    <>
+    <EventToDo />
+    </>
+  )
+}
+
+export default App
---
+++ app/vit-app/app12feb/src/EventToDo.jsx
@@ -0,0 +1,8 @@
+export default function EventToDo(){
+    return(
+        <div>
+            <h1>Event Handling</h1>
+            <button>Click Me</button>
+        </div>
+    )
+}
