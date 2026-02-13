bc93126f8a186f90ce7ae23bd1adbae1545f61be
Mukul <mukulkashyap.business@gmail.com>
2026-02-13 14:47:19 +0530

    feat: button to change coutner app name

--- app/vit-app/app12feb/src/Counter.jsx
+++ app/vit-app/app12feb/src/Counter.jsx
@@ -12,21 +12,34 @@ export default function Counter(){
     // }
 
     const [ count, setCount] = useState(0);
-
+    const [name, changeName] = useState("Counter App");
     function handleDecrement(){
         setCount((count)=> count - 1);
     }
 
     function handleIncrement(){
-        setCount((count)=> count + 1);
+        setCount(count + 1);
+    }
+
+    function handleCLick(){
+        let val = prompt("Enter new app name: ");
+        if(val.trim() !== "" && val !== null){
+            changeName(val);
+        }
+        else{
+            alert("Please don't enter an empty value.");
+        }
     }
 
     return(
         <>
-        <h1>Counter App</h1>
+        <h1>{name}</h1>
         <h3>Count : {count}</h3>
         <button onClick={handleIncrement}>+</button>
         <button onClick={handleDecrement}>-</button>
+        <br />
+        <br />
+        <button onClick={handleCLick}>Change app name</button>
         </>
     )
 }
