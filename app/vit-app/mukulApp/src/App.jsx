c5b1d97dae862eb842f54ed01092db1bb313b11a
Mukul Kashyap <mukulkashyap.business@gmail.com>
2026-04-06 14:37:00 +0530

    Dynamic routing

--- app/vit-app/mukulApp/src/App.jsx
+++ app/vit-app/mukulApp/src/App.jsx
@@ -23,14 +23,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Navbar from "./components/NavBar";
+import Details from "./components/Details"
 
 const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/abcd" element={<Home />} />
+        <Route path="/Home" element={<Home />} />
         <Route path="/About" element={<About />} />
+        <Route path="/details/:id" element={<Details/>} />
       </Routes>
     </BrowserRouter>
   );
--- app/vit-app/mukulApp/src/components/NavBar.jsx
+++ app/vit-app/mukulApp/src/components/NavBar.jsx
@@ -2,8 +2,10 @@ import {Link} from 'react-router-dom'
 const Navbar = () => {
     return(
         <nav>
-            <Link to="/abcd">Home</Link>
+            <Link to="/Home">Home</Link>
             <Link to="/About">About us</Link>
+            <Link to="/details/1">Student 1</Link>
+            <Link to="/details/2">Student 2</Link>
         </nav>
     );
 }
