351c1da26a86208959a43cb72a8676addd9113b9
Mukul Kashyap <mukulkashyap.business@gmail.com>
2026-04-13 14:26:43 +0530

    context-api

---
+++ app/vit-app/context-api-demo/src/context.jsx
@@ -0,0 +1,5 @@
+import {createContext} from 'react';
+
+export const ThemeContext = createContext();
+export const UserContext = createContext({name: "Guest"});
+export const LangContext = createContext();
