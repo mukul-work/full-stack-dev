6e7c09e5aa060791d9b160073beed739dcc3cac4
Mukul Kashyap <mukulkashyap.business@gmail.com>
2026-04-13 15:06:47 +0530

    Using reducer

--- app/vit-app/context-api-demo/src/App.jsx
+++ app/vit-app/context-api-demo/src/App.jsx
@@ -1,30 +1,61 @@
-import { useContext } from "react";
-import{ThemeContext,UserContext,LangContext} from './context';
+// import { useContext } from "react";
+// import{ThemeContext,UserContext,LangContext} from './context';
 
-const App=()=>{
+// const App=()=>{
 
-  const {theme,toggleTheme}=useContext(ThemeContext);
-  const {user,setUser}=useContext(UserContext);
-  const {lang,setLang}=useContext(LangContext);
+//   const {theme,toggleTheme}=useContext(ThemeContext);
+//   const {user,setUser}=useContext(UserContext);
+//   const {lang,setLang}=useContext(LangContext);
 
-  return(
+//   return(
    
-<div
-  style={{
-    background: theme === "light" ? "#fff" : "#333",
-    color: theme === "light" ? "#000" : "#fff",
-    padding: "20px",
-  }}
->
+// <div
+//   style={{
+//     background: theme === "light" ? "#fff" : "#333",
+//     color: theme === "light" ? "#000" : "#fff",
+//     padding: "20px",
+//   }}
+// >
+//       <p>Theme:{theme}</p>
+//       <p>User:{user.name}</p>
+//       <p>Lang:{lang}</p>
+
+//       <button onClick={toggleTheme}>ThemeChange</button>
+//       <button onClick={()=>setLang("en")}>en</button>
+//       <button onClick={()=>setLang("fr")}>fr</button>
+
+
+
+//     </div>
+//   )
+// }
+
+// export default App;
+
+import { useContext } from "react";
+import { ThemeContext, UserContext, LangContext } from './context';
+
+const App = () => {
+
+  const { state, dispatch } = useContext(ThemeContext);
+  const { theme, user, lang } = state;
+
+  return (
+
+    <div
+      style={{
+        background: theme === "light" ? "#fff" : "#333",
+        color: theme === "light" ? "#000" : "#fff",
+        padding: "20px",
+      }}
+    >
       <p>Theme:{theme}</p>
       <p>User:{user.name}</p>
       <p>Lang:{lang}</p>
 
-      <button onClick={toggleTheme}>ThemeChange</button>
-      <button onClick={()=>setLang("en")}>en</button>
-      <button onClick={()=>setLang("fr")}>fr</button>
-
-
+      <button onClick={() => dispatch({type: "TOGGLE_THEME"})}>ThemeChange</button>
+      <button onClick={() => dispatch({type: "SET_LANG", payload: "en"})}>en</button>
+      <button onClick={() => dispatch({type: "SET_LANG", payload: "fr"})}>fr</button>
 
     </div>
   )
--- app/vit-app/context-api-demo/src/Appprovider.jsx
+++ app/vit-app/context-api-demo/src/Appprovider.jsx
@@ -1,25 +1,77 @@
-import {useState} from 'react';
-import{ThemeContext,UserContext,LangContext} from './context';
+// import {useState} from 'react';
+// import{ThemeContext,UserContext,LangContext} from './context';
 
-const Appprovider=({children})=>{
-    const [theme,setTheme]=useState("light");
-    const [user,setUser]=useState({name:"guest"})
-    const [lang,setLang]=useState("en");
+// const Appprovider=({children})=>{
+//     const [theme,setTheme]=useState("light");
+//     const [user,setUser]=useState({name:"guest"})
+//     const [lang,setLang]=useState("en");
 
-    const toggleTheme=()=>
-    {
-        setTheme(prev=> prev==="light"?"dark":"light");
+//     const toggleTheme=()=>
+//     {
+//         setTheme(prev=> prev==="light"?"dark":"light");
+//     }
+// return(
+// <ThemeContext.Provider value={{theme,toggleTheme}}>
+//     <UserContext.Provider value={{user,setUser}}>
+//         <LangContext.Provider value={{lang,setLang}}>
+//             {children}
+//         </LangContext.Provider>
+
+//     </UserContext.Provider>
+// </ThemeContext.Provider>
+// )
+// }
+
+// export default Appprovider;
+
+import { useReducer } from 'react';
+import { ThemeContext, UserContext, LangContext } from './context';
+
+
+function reducer(state, action) { //REDUCER decides how state changes whereas ACTION tells what to do
+    switch (action.type) {
+        case "TOGGLE_THEME":
+            return {
+                ...state,
+                theme: state.theme === "light" ? "dark" : "light"
+            };
+
+        case "SET_USER":
+            return {
+                ...state,
+                user: action.payload,
+            };
+
+        case "SET_LANG":
+            return {
+                ...state,
+                lang: action.payload
+            };
+
+        default:
+            return state;
     }
-return(
-<ThemeContext.Provider value={{theme,toggleTheme}}>
-    <UserContext.Provider value={{user,setUser}}>
-        <LangContext.Provider value={{lang,setLang}}>
-            {children}
-        </LangContext.Provider>
+}
 
-    </UserContext.Provider>
-</ThemeContext.Provider>
-)
+const Appprovider = ({ children }) => {
+    const initialState = {
+        theme: "light",
+        user: { name: "Guest" },
+        lang: "en"
+    }
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    return (
+        <ThemeContext.Provider value={{ state, dispatch }}>
+            <UserContext.Provider value={{ state, dispatch }}>
+                <LangContext.Provider value={{ state, dispatch }}>
+                    {children}
+                </LangContext.Provider>
+
+            </UserContext.Provider>
+        </ThemeContext.Provider>
+    )
 }
 
 export default Appprovider;
